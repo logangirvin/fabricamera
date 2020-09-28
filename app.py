66c6c070dea3c1868537af0f0ce8b81200a5d599
Logan Girvin <logirvin@microsoft.com>
2020-09-28 12:36:13 -0700

    convert the png to an svg for 'reasons'

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 from flask import Flask, Response
 import qrcode
+from qrcode.image.svg import SvgPathImage
 import subprocess
 import io
+import os
 app = Flask(__name__)
-
 
 @app.route("/")
 def root():
     return "Hello, World!"
 
-@app.route("/request")
+@app.route('/request')
 def request():
     request=subprocess.run('node dist/createrequest.js', capture_output=True, cwd='./sdk')
     if request.returncode == 0:
         print('successful request generated')
-        f = io.BytesIO()
-        qrlib = qrcode.QRCode(
-            version=40,
-            error_correction=qrcode.ERROR_CORRECT_L
-        )
-        qrlib.add_data(request.stdout)
-        try:
-            qrlib.make()
-        except qrcode.exceptions.DataOverflowError as err:
-            return Response('qr too big!', status=500)
-        print('qr created')
-        qrlib.make_image().save(f)
-        return Response(f.getvalue(), mimetype='image/png')
+        return Response(request.stdout, status=200)
     else:
         print('unsuccessful request generation')
         print(request.stderr)
-        return Response('could not form request', status=500)
+        return Response(status=500)
+
+@app.route("/qr")
+def qr():
+    host = 'localhost'
+    if host in os.environ:
+        host = os.environ['host']
+    url = 'openid://vc/?request_uri={0}/request'.format(host)
+    f = io.BytesIO()
+    factory = SvgPathImage
+    qrcode.make(url, image_factory=factory).save(f)
+    return Response(f.getvalue(), mimetype='image/svg+xml')
